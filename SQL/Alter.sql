40eb8b9ffa3c22c9c4a2c1552bec53920652d242
Thuto <ThutoSiya@gmail.com>
2026-03-28 19:03:03 +0200

    Altered the persons table and added a new column

---
+++ SQL/Alter.sql
@@ -0,0 +1,8 @@
+ALTER TABLE persons
+--Add a new column to the persons table
+ADD email VARCHAR(50) NOT NULL
+
+SELECT *
+FROM persons
+
+--New Columns are appended to the end of the table by default
